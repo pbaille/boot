eab1785ac262b1bc447f771f419e4708814d09a1
Pierre Baille <pierrebaille@gmail.com>
2019-10-26 13:25:50 +0200

    init

---
+++ src/boot/core.clj
@@ -0,0 +1,32 @@
+(ns boot.core
+  (:require [boot.generics :as g]
+            [boot.types :as t]
+            [boot.prelude :as p]
+            [boot.state :as s]
+            [matches.core :as m]
+            [clojure.core :as c]))
+
+(defmacro deft [name fields & body]
+  `(do (m/deft ~name ~fields)
+       (t/tag+ ~(keyword name) [(m/->class ~name)] [])
+       (g/type+ ~(keyword name) ~@body)))
+
+(p/import-macros
+ tag+ t/tag+
+ defg g/generic
+ generic+ g/generic+)
+
+(comment
+
+  (defg greet
+    ([x] "Yo!")
+    ([x y] (str "Yo " y)))
+
+  (g/generic yop [x])
+
+  (deft num [val]
+    (greet [x] "Hey!"))
+
+  (greet (num 1))
+  (greet "iop"))
+
---
+++ src/boot/state.clj
@@ -0,0 +1,8 @@
+(ns boot.state
+  (:refer-clojure :exclude [reset!]))
+
+(def state0 {:fns {} :types {} :guards {}})
+
+(def state (atom state0))
+
+(defn reset! [] (clojure.core/reset! state state0))
