69d4431d92fa77992f5bb341965ac7be85d3d170
Pierre Baille <pierrebaille@gmail.com>
2020-02-27 13:46:23 +0100

    clean and asparagus.cross

--- src/boot/core.clj
+++ src/boot/core.clj
@@ -3,31 +3,13 @@
             [boot.types :as t]
             [boot.prelude :as p]
             [boot.state :as s]
-            [matches.core :as m]
             [clojure.core :as c]))
 
-(defmacro deft [name fields & body]
-  `(do (m/deft ~name ~fields)
-       (t/tag+ ~(keyword name) [(m/->class ~name)] [])
-       (g/type+ ~(keyword name) ~@body)))
-
 (p/import-macros
- tag+ t/tag+
+ tag+ g/tag+
  defg g/generic
  generic+ g/generic+
- defm m/defm)
+ isa t/isa)
 
-(comment
 
-  (defg greet
-    ([x] "Yo!")
-    ([x y] (str "Yo " y)))
-
-  (g/generic yop [x])
-
-  (deft num [val]
-    (greet [x] "Hey!"))
-
-  (greet (num 1))
-  (greet "iop"))
 
